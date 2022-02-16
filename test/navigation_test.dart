9ce9d679ad84e7ae3b81882db22384a5494bdfdc
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-17 00:07:48 +0700

    add test for open navigation

--- test/navigation_test.dart
+++ test/navigation_test.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  testWidgets(
+    'Open new note page',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: NoteListPage(),
+        ),
+      );
+
+      expect(find.byType(NoteListPage), findsOneWidget);
+
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(NewNotePage), findsOneWidget);
+    },
+  );
+}
