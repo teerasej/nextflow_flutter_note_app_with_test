bc92aba4cf0284d6d052a6457cb52a0a6c3e4055
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-02-17 09:11:01 +0700

    implement ui

--- lib/features/new_note/new_note_page.dart
+++ lib/features/new_note/new_note_page.dart
@@ -5,6 +5,32 @@ class NewNotePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Note'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Message',
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
